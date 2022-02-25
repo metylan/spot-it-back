0129d6860f735b25c505241105223fd11853e565
Romain <texier.r@gmail.com>
2022-02-25 11:16:52 +0100

    @add config doc and orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+		}),
+		TypeOrmModule.forRoot({
+			type: 'mariadb',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			username: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			database: process.env.DB_NAME,
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
+		AuthModule,
+		UsersModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.useGlobalPipes(new ValidationPipe);
+
+	const config = new DocumentBuilder()
+		.setTitle('Green Your Look api')
+		.setDescription('The Green Your Look API documentation')
+		.setVersion('1.0.0')
+		.addTag('greenYourLook')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('doc', app, document);
+
+	await app.listen(3000);
 }
 bootstrap();
