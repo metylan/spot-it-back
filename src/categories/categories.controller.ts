a813129e7adc8a2548c76d14a00e9c37e052d46c
Romain <texier.r@gmail.com>
2022-03-18 10:56:12 +0100

    @upd add roles autorization

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/security/roles.decorator';
+import { Role } from 'src/security/roles.enum';
+import { RolesGuard } from 'src/security/roles.guard';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -7,6 +10,8 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 export class CategoriesController {
 	constructor(private readonly categoriesService: CategoriesService) { }
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Post()
 	create(@Body() createCategoryDto: CreateCategoryDto) {
 		return this.categoriesService.create(createCategoryDto);
@@ -22,11 +27,15 @@ export class CategoriesController {
 		return this.categoriesService.findOne(+id);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
 		return this.categoriesService.update(+id, updateCategoryDto);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.categoriesService.remove(+id);
--- src/styles/styles.controller.ts
+++ src/styles/styles.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { StylesService } from './styles.service';
 import { CreateStyleDto } from './dto/create-style.dto';
 import { UpdateStyleDto } from './dto/update-style.dto';
+import { RolesGuard } from 'src/security/roles.guard';
+import { Roles } from 'src/security/roles.decorator';
+import { Role } from 'src/security/roles.enum';
 
 @Controller('styles')
 export class StylesController {
 	constructor(private readonly stylesService: StylesService) { }
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Post()
 	create(@Body() createStyleDto: CreateStyleDto) {
 		return this.stylesService.create(createStyleDto);
@@ -22,11 +27,17 @@ export class StylesController {
 		return this.stylesService.findOne(+id);
 	}
 
+
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateStyleDto: UpdateStyleDto) {
 		return this.stylesService.update(+id, updateStyleDto);
 	}
 
+
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.stylesService.remove(+id);
