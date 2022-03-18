09ef3fcfa2a7feb3a28fb0fca2e726264626a643
Romain <texier.r@gmail.com>
2022-03-18 16:23:21 +0100

    @test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
 	getHello(): string {
-		return 'Hello World! from the intertnet and auto deploys';
+		return 'Hello World!';
 	}
 }
