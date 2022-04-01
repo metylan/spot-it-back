a81870bfcde5e3f9d048892ece35ffb06adc5cc2
Romain <romain.texier@sourceamax.com>
2022-04-01 11:53:11 +0200

    @upd create look needs

--- src/looks/dto/create-look.dto.ts
+++ src/looks/dto/create-look.dto.ts
@@ -1,1 +1,14 @@
-export class CreateLookDto {}
+/* eslint-disable indent */
+import { IsDefined, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Style } from 'src/styles/entities/style.entity';
+
+export class CreateLookDto {
+	@ApiProperty()
+	@IsNotEmpty()
+	name!: string;
+
+	@ApiProperty()
+	@IsDefined()
+	style!: Style;
+}
