2d252c643af331216ccb4187384e25c93203b494
Romain <texier.r@gmail.com>
2022-04-08 11:37:28 +0200

    @upd security update

--- src/products-lists/products-lists.controller.ts
+++ src/products-lists/products-lists.controller.ts
@@ -1,32 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductsListsService } from './products-lists.service';
 import { CreateProductsListDto } from './dto/create-products-list.dto';
 import { UpdateProductsListDto } from './dto/update-products-list.dto';
+import { RolesGuard } from 'src/security/roles.guard';
+import { Roles } from 'src/security/roles.decorator';
+import { Role } from 'src/security/roles.enum';
 
 @Controller('products-lists')
 export class ProductsListsController {
 	constructor(private readonly productsListsService: ProductsListsService) { }
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Post()
 	create(@Body() createProductsListDto: CreateProductsListDto) {
 		return this.productsListsService.create(createProductsListDto);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Get()
 	findAll() {
 		return this.productsListsService.findAll();
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return this.productsListsService.findOne(+id);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateProductsListDto: UpdateProductsListDto) {
 		return this.productsListsService.update(+id, updateProductsListDto);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.productsListsService.remove(+id);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Roles } from 'src/security/roles.decorator';
+import { Role } from 'src/security/roles.enum';
+import { RolesGuard } from 'src/security/roles.guard';
 
 @Controller('products')
 export class ProductsController {
 	constructor(private readonly productsService: ProductsService) { }
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Post()
 	create(@Body() createProductDto: CreateProductDto) {
 		return this.productsService.create(createProductDto);
@@ -22,11 +27,15 @@ export class ProductsController {
 		return this.productsService.findOne(+id);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
 		return this.productsService.update(+id, updateProductDto);
 	}
 
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.productsService.remove(+id);
