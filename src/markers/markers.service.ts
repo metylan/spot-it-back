51a864cd070e2e9abe024ca2dd94c92b446cd940
Ludovic DECHAMBOUX <ludovic.dechamboux@businessdecision.com>
2022-06-08 16:18:03 +0200

    add marker back

--- src/markers/markers.service.ts
+++ src/markers/markers.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { CreateMarkerDto } from './dto/create-marker.dto';
 import { UpdateMarkerDto } from './dto/update-marker.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Marker} from "../markers/entities/marker.entity";
+import {DeleteResult, Repository} from "typeorm";
+import {User} from "../users/entities/user.entity";
+import {UpdateUserDto} from "../users/dto/update-user.dto";
 
 @Injectable()
 export class MarkersService {
+  constructor(@InjectRepository(Marker) private data: Repository<Marker>) { }
   create(createMarkerDto: CreateMarkerDto) {
-    return 'This action adds a new marker';
+    return this.data.save(createMarkerDto);
+  }
+  findAll(): Promise<Marker[]> {
+    return this.data.find();
   }
 
-  findAll() {
-    return `This action returns all markers`;
+  findOne(id: number): Promise<Marker> {
+    // @ts-ignore
+    return this.data.findOneOrFail(id).catch(() => {
+      throw new NotFoundException(id);
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} marker`;
+  async update(id: number, dto: UpdateMarkerDto): Promise<Marker> {
+    const done = await this.data.update(id, dto);
+    if (done.affected != 1) throw new NotFoundException(id);
+    return this.findOne(id);
   }
 
-  update(id: number, updateMarkerDto: UpdateMarkerDto) {
-    return `This action updates a #${id} marker`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} marker`;
+  async remove(id: number) {
+    const done: DeleteResult = await this.data.delete(id);
+    if (done.affected !== 1) throw new NotFoundException(id);
   }
 }
